2acc5ed7a1c23da3ebfe3d0a148eec42526ec897
masuryo <suryowidiyantgm@gmail.com>
2019-04-15 12:23:58 +0700

    fix softdeletes

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+	use SoftDeletes;
+	
     protected $fillable = [
     	'category_id', 'name', 'price', 'status', 'deleted_at', 'created_at', 'updated_at',
     ];
